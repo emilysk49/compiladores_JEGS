1e0ff69ea36b7fb1681ce1311cae2c28985be60b
Stephan Krug <stekrug@icloud.com>
2024-06-23 16:05:42 -0300

    feat: Adiciona análise semântica no analisador sintático

--- analisador_sintatico.py
+++ analisador_sintatico.py
@@ -1,34 +1,46 @@
-
-def analisador_sintatico(glc, predicts, actions, codigo):
-    pilha = ['$', glc.S]
-    codigo += ["$"]
-    cabecote = 1
+def analisador_sintatico(glc, predicts, actions, codigo, posicoes_lexemas_na_tabela_de_simbolos, tabela_de_simbolos):
+    pilha = [glc.cifrao, glc.S]
+    codigo += ['$']
+    cabecote = 0
 
     while pilha != []:
-        if pilha[-1] not in glc.Ps:
-            if pilha[-1] == codigo[0]:
+        if pilha[-1].name not in glc.Ps:
+            if pilha[-1].name == codigo[0]:
                 pilha.pop(-1)
                 codigo.pop(0)
                 cabecote += 1
                 continue
+            elif pilha[-1] in glc.As:
+                acao_semantica = pilha.pop(-1)
+
+                cabecote_str = str(cabecote)
+                if cabecote_str in posicoes_lexemas_na_tabela_de_simbolos:
+                    acao_semantica.lexeme = posicoes_lexemas_na_tabela_de_simbolos[cabecote_str]
+
+                acao_semantica.symbol_table = tabela_de_simbolos
+                acao_semantica.exec()
             else:
                 return codigo[0] + " Inesperado, posiçãoA " + str(cabecote)
         else:
-            if pilha[-1] == '$':
-                if codigo[0] == '$':
+            if pilha[-1] == glc.cifrao:
+                if codigo[0] == glc.cifrao.name:
                     break
                 else:
                     return codigo[0] + " Inesperado, posiçãoB " + str(cabecote)
-            if codigo[0] not in predicts[pilha[-1]]:
+
+            if codigo[0] not in predicts[pilha[-1].name]:
                 return codigo[0] + " Inesperado, posiçãoC " + str(cabecote)
-            prod = predicts[pilha[-1]][codigo[0]]
+
+            prod = predicts[pilha[-1].name][codigo[0]]
             result = actions[prod]
+
             if not result:
                 return codigo[0] + " Inesperado, posiçãoD " + str(cabecote)
             pilha.pop(-1)
 
             for s in result[::-1]:
-                if s == '&':
+                if s == glc.epsilon:
                     continue
                 pilha.append(s)
+
     return "Sucesso!"
