5dc77dcf6c793a719de33a906f8e2b1f1b449d76
Stephan Krug <stekrug@icloud.com>
2024-06-23 16:07:47 -0300

    feat: Adiciona atributos para análise semântica

--- Main.py
+++ Main.py
@@ -3,17 +3,20 @@ from analisador_sintatico import *
 from analisador_lexico import *
 
 class GLC():
-    def __init__(self, NTs, Ts, Ps, S):
+    def __init__(self, NTs, Ts, Ps, As, S, cifrao, epsilon):
         self.NTs = NTs
         self.Ts = Ts
         self.Ps = Ps
+        self.As = As
         self.S = S
+        self.cifrao = cifrao
+        self.epsilon = epsilon
 
 AL = AnalisadorLexico()
-AL.executar("testeSimples.txt")
+AL.executar("testeInsercaoTipo.txt")
 
-Trab = GLC(NTs, Terminais, Producoes, "PROGRAM")
+Trab = GLC(NTs, Terminais, Producoes, AcoesSemanticas, PROGRAM, CIFRAO, EPSILON)
 
-result = analisador_sintatico(Trab, LL1Predict, LL1Action, AL.tokens)
+result = analisador_sintatico(Trab, LL1Predict, LL1Action, AL.tokens, AL.posicoesLexemasDaTabelaDeSimbolos, AL.tabelaDeSimbolos)
 print(result)
-
+print(AL.tabelaDeSimbolos)
