29f1a87c911c6d34db2b31553a206b6edcae6a8a
Stephan Krug <stekrug@icloud.com>
2024-07-03 21:34:42 -0300

    Imprime saídas esperadas da compilação

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import os
+import pprint
 from Gramatica_em_dicionario import *
 from analisador_sintatico import *
 from analisador_lexico import *
+
+print("Integrantes: Emily Sayuri Kiba, Gabriel Turatti Andrade, Julien Hervot de Mattos Vaz e Stephan Krug\n")
 
 class GLC():
     def __init__(self, NTs, Ts, Ps, As, S, cifrao, epsilon):
@@ -23,7 +26,27 @@ Trab = GLC(NTs, Terminais, Producoes, AcoesSemanticas, PROGRAM, CIFRAO, EPSILON)
 lista_de_nodos = []
 result = analisador_sintatico(Trab, LL1Predict, LL1Action, AL.tokens, AL.posicoesLexemasDaTabelaDeSimbolos, AL.tabelaDeSimbolos, lista_de_nodos)
 
-# for nodo in lista_de_nodos:
-    # print(nodo)
-print(result)
-print(AL.tabelaDeSimbolos)
+
+if result == "Sucesso!":
+    print("=== Árvores de expressão ===")
+    for i, raiz in enumerate(lista_de_nodos):
+        print(f"Árvore {i}:")
+        print(raiz)
+    print("============================\n")
+
+    print("==== Tabela de símbolos ====")
+    pprint.pp(AL.tabelaDeSimbolos)
+    print("============================\n")
+
+    print("======== Compilação ========")
+    print("Expressões aritméticas válidas.")
+    print("Declarações de variáveis por escopo válidas.")
+    print("Todo break está no escopo de um for.")
+
+    print("============================\n")
+
+    print("=== Código intermediário ===")
+    print("Coming soon...")
+    print("============================")
+else:
+    print(result)
